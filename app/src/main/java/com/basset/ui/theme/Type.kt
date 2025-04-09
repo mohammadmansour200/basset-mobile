3f0d8a37f50f6c4a1feda66e949c136369eb0c8f
mohammadmansour200 <abdultawab940@gmail.com>
2025-04-09 11:12:15 +0300

    feat: Added new font

--- app/src/main/java/com/basset/ui/theme/Type.kt
+++ app/src/main/java/com/basset/ui/theme/Type.kt
@@ -1,6 +1,41 @@
 package com.basset.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.basset.R
 
-// Set of Material typography styles to start with
-val Typography = Typography()
+val theYearOfTheCamel = FontFamily(
+    Font(
+        resId = R.font.theyearofhandicrafts_regular,
+        weight = FontWeight.Normal,
+    ),
+    Font(
+        resId = R.font.theyearofhandicrafts_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.theyearofhandicrafts_semibold,
+        weight = FontWeight.SemiBold,
+    ),
+    Font(
+        resId = R.font.theyearofhandicrafts_bold,
+        weight = FontWeight.Bold,
+    ),
+)
+
+val baseline = Typography()
+
+val Typography = Typography(
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = theYearOfTheCamel),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = theYearOfTheCamel),
+    bodySmall = baseline.bodySmall.copy(fontFamily = theYearOfTheCamel),
+    labelLarge = baseline.labelLarge.copy(fontFamily = theYearOfTheCamel),
+    labelMedium = baseline.labelMedium.copy(fontFamily = theYearOfTheCamel),
+    labelSmall = baseline.labelSmall.copy(fontFamily = theYearOfTheCamel),
+    titleLarge = baseline.titleLarge.copy(fontFamily = theYearOfTheCamel),
+    titleMedium = baseline.titleMedium.copy(fontFamily = theYearOfTheCamel),
+    titleSmall = baseline.titleSmall.copy(fontFamily = theYearOfTheCamel),
+)
+
