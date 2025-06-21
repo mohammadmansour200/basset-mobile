cf5bebb9eeeae31818a24c286a95549607d7d1cc
mohammadmansour200 <abdultawab940@gmail.com>
2025-06-21 06:41:58 +0300

    feat: Implement convert operation
    This commit introduces the convert operation, allowing users to change the format of their images.
    
    Key changes include:
    - Updated `UriToBitmap` to optionally disable scaling.
    - Added new string resources for the convert operation in both English and Arabic.

--- app/src/main/java/com/basset/operations/data/android/UriToBitmap.kt
+++ app/src/main/java/com/basset/operations/data/android/UriToBitmap.kt
@@ -8,29 +8,44 @@ import android.net.Uri
 import android.os.Build
 import androidx.core.graphics.scale
 
-fun Uri.toBitmap(targetWidth: Int, targetHeight: Int, context: Context): Bitmap {
+fun Uri.toBitmap(
+    targetWidth: Int?,
+    targetHeight: Int?,
+    context: Context,
+    scaled: Boolean = true
+): Bitmap {
+    val isScaled = scaled && targetWidth != null && targetHeight != null
     return if (Build.VERSION.SDK_INT < 28) {
         val input = context.contentResolver.openInputStream(this)
         val options = BitmapFactory.Options().apply {
             inJustDecodeBounds = true
             BitmapFactory.decodeStream(input, null, this)
             input?.close()
-
-            val sampleSizeW = outWidth / targetWidth
-            val sampleSizeH = outHeight / targetHeight
-            inSampleSize = maxOf(1, minOf(sampleSizeW, sampleSizeH))
+            if (isScaled) {
+                val sampleSizeW = outWidth / targetWidth
+                val sampleSizeH = outHeight / targetHeight
+                inSampleSize = maxOf(1, minOf(sampleSizeW, sampleSizeH))
+            } else {
+                inSampleSize = 1
+            }
             inJustDecodeBounds = false
         }
 
         val input2 = context.contentResolver.openInputStream(this)
         val decoded = BitmapFactory.decodeStream(input2, null, options)
         input2?.close()
-        decoded!!.scale(targetWidth, targetHeight)
+        if (isScaled) {
+            decoded!!.scale(targetWidth, targetHeight)
+        } else {
+            decoded!!
+        }
     } else {
         val source = ImageDecoder.createSource(context.contentResolver, this)
         ImageDecoder.decodeBitmap(source) { decoder, info, _ ->
             decoder.setAllocator(ImageDecoder.ALLOCATOR_SOFTWARE)
-            decoder.setTargetSize(targetWidth, targetHeight)
+            if (isScaled) {
+                decoder.setTargetSize(targetWidth, targetHeight)
+            }
         }
     }
 }
