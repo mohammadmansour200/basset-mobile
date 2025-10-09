f873fa5b6d0e39f9b718903ee3586933ec6b3ae4
mohammadmansour200 <abdultawab940@gmail.com>
2025-10-09 13:58:42 +0300

    build: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.basset.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
